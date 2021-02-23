5fdbefe7c2051581025cd26eb30ad95a417e0c05
ristoml <leivo.risto@gmail.com>
2021-02-23 22:42:12 +0200

    initial commit

---
+++ vue-app/src/components/InputField.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+        <textarea v-model="title"
+        placeholder="Enter title here"
+        rows="1"
+        cols="80"></textarea><br>
+        <textarea v-model="message" 
+        placeholder="paste your text here"
+        rows="30"
+        cols="80"></textarea>
+        <br>
+        <input type="checkbox" id="checkbox" v-model="unlisted">        
+        <label for="checkbox">Unlisted</label>
+        <br>
+        <button v-on:click="sendPaste">Send</button>
+    </div>    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'InputField',
+  props: {
+    msg: String,
+  },
+  data() {
+      return {
+        title: [],
+        message: [],
+        combinedMsg: [],
+        unlisted: false
+      }     
+  },
+  methods: {
+      sendPaste() {        
+        axios({
+        method: 'post',
+        url: 'localhost:3000/api/pastes',
+        data: {
+            title: this.title,
+            content: this.message,
+            unlisted: this.unlisted
+        }
+        })        
+        .then(function (response) {                
+            console.log(response)
+            this.title = "";
+            this.message = "";
+            this.unlisted = false;
+        })
+        .catch(function (error) {
+            console.log(error)
+        });
+      }
+  } 
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InputField/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import InputField from '@/components/InputField.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    InputField,
   },
 };
+</script>
 </script>
